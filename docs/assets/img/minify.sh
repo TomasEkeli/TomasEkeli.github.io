bb13d3808fadf51fed44d5d9138c40221643ecea
Tomas Ekeli <te@davidhorn.com>
2023-12-15 14:44:44 +0000

    resized images

--- docs/assets/img/minify.sh
+++ docs/assets/img/minify.sh
@@ -6,4 +6,5 @@ find . -type f -regex ".*\.\(jpg\|jpeg\|png\)" -exec rm {}  \; -print
 find . -maxdepth 1 -type f -regex ".*\.\(webp\)" -exec convert {} -resize 1024x {}  \; -print
 
 # make small versions for the post listing
+rm *-small.webp
 find . -maxdepth 1 -type f -regex ".*\.\(webp\)" -exec bash -c 'convert "$0" -resize 200x "${0%.*}-small.${0##*.}"' {} \; -print
